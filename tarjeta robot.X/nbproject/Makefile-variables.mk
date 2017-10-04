9c19193f8fe928736484c233e66357e58acb19fb
Oscar024 <or.carvajal.ortiz@gmail.com>
2017-10-04 14:54:41 -0700

    Initial commit

---
+++ tarjeta robot.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=tarjeta_robot.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/tarjeta_robot.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=tarjetarobot.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/tarjetarobot.x.tar
